3ca86bfe1ccb6dac71dcd679f0a8e91917c1254d
Saurabh Gattani <saurabh.gattani@gmail.com>
2021-08-24 00:14:42 +1000

    added support for eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "root": true,
+  "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module"
+  },
+  "extends": [
+    "plugin:vue/essential",
+    "plugin:prettier/recommended",
+    "eslint:recommended"
+  ],
+}
